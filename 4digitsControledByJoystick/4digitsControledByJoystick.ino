b38d3e419faf77efdea050669b76bcd9be1fb66e
Paul <paulconst7000@gmail.com>
2021-11-16 06:28:07 +0200

    hm4 draft

---
+++ 4digitsControledByJoystick/4digitsControledByJoystick.ino
@@ -0,0 +1,177 @@
+const int dataPin = 12; // DS
+const int latchPin = 11; // STPC
+const int clockPin = 10; // SHCP
+
+const int xPin = A0; 
+const int yPin = A1;
+const int swPin = 2;
+
+const int joyMoveMin = 300;
+const int joyMoveMax = 700;
+bool joyMoved = LOW;
+bool joyLeft = LOW;
+bool joyRight = LOW;
+bool joyUP = LOW;
+bool joyDown = LOW;
+int currentDigit = 0;
+bool digitSelected = LOW;
+int shownDigits[] = {
+  0, 0, 0, 0
+};
+int shownNumber = 1;
+
+bool reading = HIGH;
+bool prevReading = HIGH;
+int lastBounce = 0;
+const int bounceDelay = 50;
+
+int xValue = 0;
+int yValue = 0;
+int swState = HIGH;
+
+const int segD1 = 7;
+const int segD2 = 6;
+const int segD3 = 5;
+const int segD4 = 4;
+
+const int segmentDigits[] = {
+  segD1, segD2, segD3, segD4
+};
+
+const int segmentsCount = 4;
+
+int digitArray[16] = {
+//A B C D E F G DP 
+  B11111100, // 0
+  B01100000, // 1
+  B11011010, // 2
+  B11110010, // 3
+  B01100110, // 4
+  B10110110, // 5
+  B10111110, // 6
+  B11100000, // 7
+  B11111110, // 8
+  B11110110, // 9
+  B11101110, // A
+  B00111110, // B
+  B10011100, // C
+  B01111010, // D
+  B10011110, // E
+  B10001110  // F
+};
+void setup() {
+  pinMode(dataPin, OUTPUT);
+  pinMode(latchPin, OUTPUT);
+  pinMode(clockPin, OUTPUT);
+
+  for(int i = 0; i < segmentsCount; i++) {
+    pinMode(segmentDigits[i], OUTPUT);
+    digitalWrite(segmentDigits[i], LOW);
+
+    pinMode(xPin, INPUT);
+    pinMode(yPin, INPUT);
+    pinMode(swPin, INPUT_PULLUP); 
+    attachInterrupt(digitalPinToInterrupt(swPin), unBlinkDP, CHANGE);
+  }
+  Serial.begin(9600);
+}
+
+void loop() {
+  blinkDP();
+  xValue = analogRead(xPin);
+  yValue = analogRead(yPin);
+    
+  reading = digitalRead(swPin);
+
+  if (reading != prevReading) {
+  lastBounce = millis();
+  }
+
+  if(millis() - lastBounce > bounceDelay) {
+    if(reading != swState) {
+      swState = reading;
+      if(reading == LOW) {
+        digitSelected = !digitSelected;
+  }
+    }
+  }
+  prevReading = reading;
+
+  if(xValue > joyMoveMin && xValue < joyMoveMax && yValue > joyMoveMin && yValue < joyMoveMax) {
+    joyMoved = LOW;
+    //Serial.println("neutral");
+  }
+  if(joyMoved == LOW) {
+    if(xValue < joyMoveMin && !digitSelected) {
+      joyLeft = HIGH;
+      joyMoved = HIGH;
+      if(currentDigit > 0){
+        currentDigit--;
+      }
+    }
+    if(xValue > joyMoveMax && !digitSelected) {
+      joyRight = HIGH;
+      joyMoved = HIGH;
+      if(currentDigit < 3){
+        currentDigit++;
+      }
+    }
+    if(yValue < joyMoveMin && digitSelected) {
+      joyDown = HIGH;
+      joyMoved = HIGH;
+      if(shownDigits[currentDigit] > 0){
+        shownDigits[currentDigit]--;
+      }
+    }
+    if(yValue > joyMoveMax && digitSelected) {
+      joyUP = HIGH;
+      joyMoved = HIGH;
+      if(shownDigits[currentDigit] < 9){
+        shownDigits[currentDigit]++;
+      }
+    }
+  }
+
+  for(int i = 0; i < 4; i++) {
+    shownNumber *= 10;
+    shownNumber += shownDigits[i];
+  }
+  //Serial.println(shownNumber);
+  writeNumber(shownNumber);
+  shownNumber = 1;
+}
+
+void writeNumber(int number) {
+  int currentNumber = number;
+  int lastDigit;
+  int displayDigit = 0;
+  while (currentNumber > 0) {
+    lastDigit = currentNumber % 10;
+    showDigit(displayDigit);
+    writeReg(digitArray[lastDigit]);
+    displayDigit ++;
+    currentNumber /= 10;
+    delay(5);
+  }
+}
+
+void showDigit(int digitNumber) {
+  for(int i = 0; i< segmentsCount; i++) {
+    digitalWrite(segmentDigits[i], HIGH);
+  }
+  digitalWrite(segmentDigits[digitNumber], LOW);
+}
+
+void writeReg(int digit) {
+  digitalWrite(latchPin, LOW);
+  shiftOut(dataPin, clockPin, MSBFIRST, digit);
+  digitalWrite(latchPin, HIGH);
+}
+
+void blinkDP() {
+  Serial.println("blink");
+}
+
+void unBlinkDP() {
+  Serial.println(currentDigit);
+}
